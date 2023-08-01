02983607a801ce07e27b42011a0da337a26ec33c
AnujSingh13 <anujj.singh.13@gmail.com>
2023-08-02 01:59:30 +0530

    app Commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 import os
- 
+
 # Sidebar contents
 with st.sidebar:
     st.title('LLM Chatbot')
@@ -21,20 +21,20 @@ with st.sidebar:
     - [LangChain](https://python.langchain.com/)
     - [OpenAI](https://platform.openai.com/docs/models)
     - [LLM Models](https://www.geeksforgeeks.org/large-language-model-llm/) 
- 
+
     ''')
- 
+
 def main():
     st.header("Chat with PDF 💬")
 
     load_dotenv()
 
-     # Load the API key from the environment variable
+    # Load the API key from the environment variable
     embeddings = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
- 
+
     # upload a PDF file
     pdf = st.file_uploader("Upload your PDF", type='pdf')
- 
+
     # st.write(pdf)
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
@@ -42,19 +42,19 @@ def main():
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
- 
+
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
             chunk_overlap=200,
             length_function=len
             )
         chunks = text_splitter.split_text(text=text)
- 
+
         # # embeddings
         store_name = pdf.name[:-4]
         st.write(f'{store_name}')
         # st.write(chunks)
- 
+
         if os.path.exists(f"{store_name}.pkl"):
             with open(f"{store_name}.pkl", "rb") as f:
                 VectorStore = pickle.load(f)
@@ -68,10 +68,10 @@ def main():
         # Accept user questions/query
         query = st.text_input("Ask questions about your PDF file:")
         # st.write(query)
- 
+
         if query:
             docs = VectorStore.similarity_search(query=query, k=3)
- 
+
             llm = OpenAI()
             chain = load_qa_chain(llm=llm, chain_type="stuff")
             with get_openai_callback() as cb:
@@ -79,6 +79,6 @@ def main():
                 print(cb)
                 
             st.write(response)
- 
+
 if __name__ == '__main__':
     main()
